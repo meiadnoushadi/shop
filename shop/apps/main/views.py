659b8fffdc6a4abe8ab16abe5d8a6f045a006b11
meiadnoushadi <meiadnoushadi@gmail.com>
2022-12-11 17:29:31 +0330

    access for media and sample content in page home

--- shop/apps/main/views.py
+++ shop/apps/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
 def index(request):
     context={
-        
+       'media_url':settings.MEDIA_URL 
     }
     return render(request,'main/index.html',context)
