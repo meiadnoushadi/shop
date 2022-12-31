00de2a7eceda12e0d529d62a0a901cd052fc1246
MEIAD <meiadnoushadi@gmail.com>
2022-12-31 14:29:34 +0330

    change name appps to apps.blog.apps.BlogConfig
    for create models in application blog
    active run migrate order

--- shop/apps/blog/models.py
+++ shop/apps/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Auther(models.Model):
+    name=models.CharField(max_length=30)
+    family=models.CharField(max_length=30)
+    sulg=models.SlugField(max_length=100)
+    age=models.IntegerField(default=0)
+    is_active=models.BooleanField(default=True)
+    register_date=models.DateTimeField(default=timezone.now)
+    email=models.EmailField(max_length=100)
